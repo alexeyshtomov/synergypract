77a6268a0e8f901d9a204f3c53d11e4ce1b65b76
Alexey Shtomov <alexeyshtomov2001@gmail.com>
2024-07-30 21:01:09 +0300

    new_Commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-new_list = [1, 2, 3]
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return f"{self.name} издает звук"
+
+
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} говорит Гав-гав!"
+
+
+animal = Animal("Животное")
+print(animal.speak())
+
+dog = Dog("Барсик")
+print(dog.speak())
